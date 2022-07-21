0d6f4947419ded3bc21ce9ca711b1c2e271c4c95
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-07-21 09:51:14 +0200

    rebuild components

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -18,12 +18,6 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Id wybranego bohatera: ${hero.id}`);
-  }
-
   getHeroes(): void{
     this.heroService.getHeroes()
     .subscribe(response => this.heroes = response)
